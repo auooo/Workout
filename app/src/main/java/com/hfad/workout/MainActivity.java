40d1bf3907cc6894bf42ae8c180256ed1d21ac37
auooo <mh245500@gmail.com>
2015-12-26 15:15:39 +0800

    Initial commit

---
+++ app/src/main/java/com/hfad/workout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hfad.workout;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WorkoutDetailFragment frag = (WorkoutDetailFragment)
+                getFragmentManager().findFragmentById(R.id.detail_frag);
+        frag.setWorkoutId(1);
+    }
+}
---
+++ app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -0,0 +1,42 @@
+package com.hfad.workout;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutDetailFragment extends Fragment {
+    private long workoutId;
+
+    // It's called when Android needs the fragment's layout
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+
+    public void setWorkoutId(long workoutId) {
+        this.workoutId = workoutId;
+    }
+}
