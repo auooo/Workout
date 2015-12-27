8e9e0d4b344577725fbe330f8caff7493f8ee462
auooo <mh245500@gmail.com>
2015-12-27 22:21:45 +0800

    add the inner interface

--- app/src/main/java/com/hfad/workout/WorkoutListFragment.java
+++ app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -1,22 +1,44 @@
 package com.hfad.workout;
 
 import android.app.ListFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class WorkoutListFragment extends ListFragment {
+
+    interface WorkoutListListener {
+        void itemClicked(long id);
+    }
+
+    private WorkoutListListener listListener;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         String[] names = new String[Workout.workouts.length];
         for (int i = 0; i < names.length; i++) {
             names[i] = Workout.workouts[i].getName();
         }
-        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(inflater.getContext(),
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(inflater.getContext(),
                 android.R.layout.simple_list_item_1, names);
         setListAdapter(listAdapter);
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listListener = (WorkoutListListener)context;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        if (listListener != null) {
+            listListener.itemClicked(id);
+        }
     }
 }
